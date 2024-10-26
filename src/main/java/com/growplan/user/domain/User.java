9ee452bdde275b3a4f1020cf3e3cb04c9558a77f
Hagoeun <sera1193@naver.com>
2024-10-26 15:49:08 +0900

    [FEAT] user 엔티티 생성

---
+++ src/main/java/com/growplan/user/domain/User.java
@@ -0,0 +1,55 @@
+package com.growplan.user.domain;
+
+import com.growplan.child.domain.UserChild;
+import com.growplan.common.BaseEntity;
+import com.growplan.common.type.StatusType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor(access = PROTECTED)
+@SQLDelete(sql = "UPDATE member SET isValid = 'DELETED' where id = ?")
+@SQLRestriction("status = 'ACTIVE'")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String birthdate;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private StatusType isValid;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<UserChild> userChildren;
+
+    @OneToOne(orphanRemoval = true, cascade = CascadeType.ALL)
+    private UserSign userSign;
+}
---
+++ src/main/java/com/growplan/user/domain/UserSign.java
@@ -0,0 +1,28 @@
+package com.growplan.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class UserSign {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
