447009d4edc2016f1b907cab08976248a2bec4c4
Hagoeun <sera1193@naver.com>
2024-10-26 13:12:04 +0900

    first commit

---
+++ src/main/java/com/growplan/GrowplanApplication.java
@@ -0,0 +1,13 @@
+package com.growplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/growplan/GrowplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
