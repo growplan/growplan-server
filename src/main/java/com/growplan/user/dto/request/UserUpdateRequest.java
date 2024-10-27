396a6e380a09a1bed6e71bf9978eabcc515ec580
Hagoeun <sera1193@naver.com>
2024-10-27 17:22:07 +0900

    [FEAT] 회원 업데이트 기능 추가

---
+++ src/main/java/com/growplan/user/dto/request/UserUpdateRequest.java
@@ -0,0 +1,25 @@
+package com.growplan.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserUpdateRequest {
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    private String name;
+
+    @NotBlank(message = "생년월일을 입력해주세요.")
+    private String birthdate;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "전화번호를 입력해주세요.")
+    private String number;
+}
