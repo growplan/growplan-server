ddf81f848180d2674da0e594d49fdc7b031f8fed
Hagoeun <sera1193@naver.com>
2024-10-27 14:48:22 +0900

    [FEAT] jwt 설정

--- src/main/java/com/growplan/common/code/ExceptionCode.java
+++ src/main/java/com/growplan/common/code/ExceptionCode.java
@@ -9,6 +9,11 @@ public enum ExceptionCode {
 
     INVALID_REQUEST(1000, "올바르지 않은 요청입니다."),
 
+    INVALID_REFRESH_TOKEN(2001, "유효하지 않은 RefreshToken입니다."),
+    INVALID_ACCESS_TOKEN(2002, "유효하지 않은 AccessToken입니다."),
+    EXPIRED_REFRESH_TOKEN(2003, "만료된 AccessToken입니다."),
+    EXPIRED_ACCESS_TOKEN(2004, "만료된 RefreshToken입니다."),
+
     INTERNAL_SERVER_ERROR(9999, "서버에서 에러가 발생하였습니다.");
 
     private final int code;
