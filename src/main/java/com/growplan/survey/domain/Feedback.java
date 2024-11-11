2ee372bb523c4ee2587c601861d2c1e19f0d9e3c
Hagoeun <sera1193@naver.com>
2024-11-11 16:57:05 +0900

    [FEAT] feedback 엔티티 추가

---
+++ src/main/java/com/growplan/survey/domain/Feedback.java
@@ -0,0 +1,32 @@
+package com.growplan.survey.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer range;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "survey_group_id", nullable = false)
+    private SurveyGroup surveyGroup;
+}
