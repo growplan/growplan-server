2f665f290818b4f78263ef479df58d69fe8b8307
Hagoeun <sera1193@naver.com>
2024-11-24 10:14:19 +0900

    [FEAT] 로그인 api 작성

--- src/main/java/com/growplan/login/dto/request/LoginRequest.java
+++ src/main/java/com/growplan/login/dto/request/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.growplan.login.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -12,8 +13,10 @@ import lombok.NoArgsConstructor;
 public class LoginRequest {
 
     @NotNull(message = "아이디를 입력해주세요.")
+    @Schema(description = "사용자 아이디", example = "user123")
     private String username;
 
     @NotNull(message = "비밀번호를 입력해주세요.")
+    @Schema(description = "사용자 비밀번호", example = "password123!")
     private String password;
 }
--- src/main/java/com/growplan/login/dto/response/LoginResponse.java
+++ src/main/java/com/growplan/login/dto/response/LoginResponse.java
@@ -1,5 +1,6 @@
 package com.growplan.login.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LoginResponse {
 
+    @Schema(description = "액세스 토큰", example = "eyJhbGciOiJIUzI1...")
     private final String accessToken;
+
+    @Schema(description = "리프레시 토큰", example = "dXNlcjEyMy5yZWZyZX==...")
     private final String refreshToken;
 
     public static LoginResponse of(
