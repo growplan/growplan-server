b94583810c3065e9a428de9a7e8bae845af0d43f
Hagoeun <sera1193@naver.com>
2024-11-27 12:18:43 +0900

    [FIX] test 어노테이션 삭제

--- src/test/java/com/growplan/GrowplanApplicationTests.java
+++ src/test/java/com/growplan/GrowplanApplicationTests.java
@@ -3,7 +3,7 @@ package com.growplan;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GrowplanApplicationTests {
 
 	@Test
