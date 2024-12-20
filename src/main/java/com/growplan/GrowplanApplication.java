3092e44001534480db5e840b756e758add97b21f
Hagoeun <sera1193@naver.com>
2024-12-20 23:58:38 +0900

    [FEAT] swagger cors

--- src/main/java/com/growplan/GrowplanApplication.java
+++ src/main/java/com/growplan/GrowplanApplication.java
@@ -1,5 +1,7 @@
 package com.growplan;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class GrowplanApplication {
 
     public static void main(String[] args) {
